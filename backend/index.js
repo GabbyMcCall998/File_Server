57aeb9ccd4c67b8395bb61bc7a96f6f11a983e6f
Gabriel Andoh Acquah <gabrielandoh998@gmail.com>
2024-06-07 00:38:48 +0000

    feat: add file upload functionality

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,42 @@
+const port = 5000;
 const express= require('express')
 const auth = require("./routes/auth")
 const mongoose = require("mongoose")
 const path = require("path")
 const cors = require("cors")
+const multer = require("multer")
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-
 app.use("/auth", auth);0
+
+// Dataabase Connection with MongoDB
+mongoose.connect("mongodb+srv://gabrielandoh998:Godneverfails998@cluster0.qqwl67e.mongodb.net/File_Server")
+
+
+// File Storage engine
+const storage= multer.diskStorage({
+    destination:'./upload/files',
+    filename:(req,file,cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+
+//creating the upload function
+const upload = multer({storage:storage})
+
+//creating upload endpoint
+app.use('/files',express.static('upload/files'))
+app.post("/upload",upload.single('record'),(req,res)=>{
+    res.json({
+        success:1,
+        file_url:`http://localhost:${port}/files/${req.file.filename}`
+    })
+})
+
 
 
 
@@ -19,6 +46,6 @@ app.get("/", (req,res) =>{
 })
 
 
-app.listen(5000, ()=>{
+app.listen(port, ()=>{
     console.log('The app is running on port 5000')
 })
