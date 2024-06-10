4dee0cb3acc1ae7a7a71f6783af4050325009d54
Gabriel Andoh Acquah <gabrielandoh998@gmail.com>
2024-06-10 01:54:41 +0000

    Updated schemas in file.js

--- backend/models/File.js
+++ backend/models/File.js
@@ -2,15 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const fileSchema = new Schema({
+  id: { type: Number, unique: true }, 
   title: { type: String, required: true },
   description: { type: String },
   file_download_url: { type: String, required: true },
-  category: { type: String, required: true }, 
+  category: { type: String, required: true },
   created_at: { type: Date, default: Date.now },
-  downloads: { type: Number, default: 0 },   // Track the number of downloads
-  emails_sent: { type: Number, default: 0 }  // tracks the number of emails
+  downloads: { type: Number, default: 0 }, // Track the number of downloads
+  emails_sent: { type: Number, default: 0 } // Track the number of emails
 });
 
 
-module.exports = mongoose.model('File', fileSchema);
+const File = mongoose.model('File', fileSchema);
 
+module.exports = File;
