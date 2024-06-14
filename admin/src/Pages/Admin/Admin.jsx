6b687587c62b848319058da7aae1d070122cdcc2
Gabriel Andoh Acquah <gabrielandoh998@gmail.com>
2024-06-14 13:34:21 +0000

    Mounted sidebar.jsx and addfile.jsx on admin.jsx

--- admin/src/Pages/Admin/Admin.jsx
+++ admin/src/Pages/Admin/Admin.jsx
@@ -1,10 +1,17 @@
+import Sidebar from '../../Component/Sidebar/Sidebar';
 import './Admin.css'
-
+import { Routes,Route } from 'react-router-dom';
+import Listfile from '../../Component/Listfile/Listfile';
+import Addfile from '../../Component/Addfile/Addfile';
 
 const Admin = () => {
     return (
          <div className="admin">
-            
+            <Sidebar/>
+            <Routes>
+               <Route path='/addfile' element={<Addfile/>}/>
+               <Route path='/listfile' element={<Listfile/>}/>
+            </Routes>
         </div>
      );
 }
