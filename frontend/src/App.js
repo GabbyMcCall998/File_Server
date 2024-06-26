dd79cae40b3accb73a8d9aaf220d6182fdac5873
Gabriel Andoh Acquah <gabrielandoh998@gmail.com>
2024-06-26 12:44:48 +0000

    Mounted new components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,9 @@ import Home from './Pages/Home';
 import Document from './Pages/Document';
 import LoginSignup from './Pages/LoginSignup';
 import SignupLogin from './Pages/SignupLogin';
+import VerifyEmailPage from './Components/VerifyEmailPage/VerifyEmailPage';
+import ForgotPassword from './Components/ForgotPassword/ForgotPassword';
+import ResetPassword from './Components/ResetPassword/ResetPassword';
 
 
 function App() {
@@ -21,9 +24,12 @@ function App() {
     <Route path='/document' element={<Document/>}/>
       <Route path=':documentId' element={<Document/>}/>
     <Route path='/login' element={<LoginSignup/>}/>
-    <Route path='/signup' element={<SignupLogin />} />
+    <Route path='/signup' element={<SignupLogin />} />  
+    <Route path='/verify-email/:id/verify/:Vtoken' element={<VerifyEmailPage />} />
+    <Route path='/forgot-password' element={<ForgotPassword/>}/>
+    <Route path='/reset-password/:id/:token' element={<ResetPassword/>}/>
 
-  </Routes>
+    </Routes>
   <Footer/>
 </BrowserRouter>
 
