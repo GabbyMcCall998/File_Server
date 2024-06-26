3187ba1ad13eb08a87278d6c1f43542842a5731f
Gabriel Andoh Acquah <gabrielandoh998@gmail.com>
2024-06-26 12:37:18 +0000

    Modified user collection,added indexing for faster lookup

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,9 +3,14 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
-  password_hash: { type: String, required: true },
+  password: { type: String, required: true },
   is_verified: { type: Boolean, default: false },
   created_at: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('User', userSchema);
+// Indexing email field for faster lookup
+userSchema.index({ email: 1 });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
