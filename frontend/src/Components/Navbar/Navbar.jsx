8d6ca080d72c5c59d6921f51f4275e7f5532907a
Gabriel Andoh Acquah <gabrielandoh998@gmail.com>
2024-07-03 00:52:29 +0000

    Modified navbar,added a search bar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,10 @@
-
 import React, { useState } from 'react';
 import './Navbar.css'; // Import CSS file for styling
 import { Link } from 'react-router-dom';
+import SearchBar from '../SearchFiles/SearchFiles';
 
 const Navbar = () => {
-
-    const [menu,setMenu] = useState("Home"); 
-
-
+    const [menu, setMenu] = useState("home"); 
 
     return (  
         <div className='Navbar'>
@@ -15,25 +12,25 @@ const Navbar = () => {
                 <p>PaperTrail</p>
             </div>
             <ul className='nav-menu'>
-                <li onClick={()=>{setMenu("home")}}><Link to='/' style={{textDecoration:'none'}} >Home</Link>{menu==="home"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("document")}}><Link to='/document' style={{textDecoration:'none'}} >Document</Link>{menu==="document"?<hr/>:<></>}</li>
-                <div className='search-container'>
-                <form  className='search' action="/search" method="get">
-                    <div className="search-wrapper">
-                    <input  type="text" placeholder="Search..." name="search" />
-                    <button type="submit">Search</button>
-                    </div>
-                </form>
-            </div>
+                <li onClick={() => setMenu("home")}>
+                    <Link to='/' style={{textDecoration:'none'}}>Home</Link>
+                    {menu === "home" && <hr />}
+                </li>
+                <li onClick={() => setMenu("document")}>
+                    <Link to='/document' style={{textDecoration:'none'}}>Document</Link>
+                    {menu === "document" && <hr />}
+                </li>
             </ul>
+                <SearchBar />
             <div className='login-button'>
                 {localStorage.getItem('token') ?
-                <button onClick={() => { localStorage.removeItem('token'); window.location.replace("/"); }}>Logout</button>
-                :<Link to='/login'><button>Login</button></Link>
+                    <button onClick={() => { localStorage.removeItem('token'); window.location.replace("/"); }}>Logout</button>
+                    :
+                    <Link to='/login'><button>Login</button></Link>
                 }
-
             </div>
         </div>
     );
 }
+
 export default Navbar;
