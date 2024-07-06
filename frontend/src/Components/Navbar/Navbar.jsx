2ea95465e0d4abadd3c0e7e7932e9b45a5e77a27
Gabriel Andoh Acquah <gabrielandoh998@gmail.com>
2024-07-06 19:16:16 +0000

    Added admin panel button

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Navbar.css'; // Import CSS file for styling
 import { Link } from 'react-router-dom';
 import SearchBar from '../SearchFiles/SearchFiles';
 
 const Navbar = () => {
-    const [menu, setMenu] = useState("home"); 
+    const [menu, setMenu] = useState("home");
+    const [isAdmin, setIsAdmin] = useState(false); // State to track admin status
 
-    return (  
+    useEffect(() => {
+        // Check if user is admin based on role in localStorage
+        const role = localStorage.getItem('role');
+        if (role === 'admin') {
+            setIsAdmin(true);
+        } else {
+            setIsAdmin(false);
+        }
+    }, []);
+
+    return (
         <div className='Navbar'>
             <div className='brand'>
-                <p>PaperTrail</p>
+                <p className='trail'>PaperTrail</p>
             </div>
             <ul className='nav-menu'>
                 <li onClick={() => setMenu("home")}>
@@ -21,7 +32,7 @@ const Navbar = () => {
                     {menu === "document" && <hr />}
                 </li>
             </ul>
-                <SearchBar />
+            <SearchBar />
             <div className='login-button'>
                 {localStorage.getItem('token') ?
                     <button onClick={() => { localStorage.removeItem('token'); window.location.replace("/"); }}>Logout</button>
@@ -29,6 +40,7 @@ const Navbar = () => {
                     <Link to='/login'><button>Login</button></Link>
                 }
             </div>
+            {isAdmin && localStorage.getItem('token') &&  <Link to='/admin' ><button >Dashboard</button></Link>}
         </div>
     );
 }
